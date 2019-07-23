86f86edbbb29a7148b18e506af55cf62a9521227
danX-4q <chenxd@bankledger.com>
2019-07-23 14:01:53 +0800

    add: merge safe.oracle@v1.5.0-feature-safecode, compile successfully.

---
+++ contracts/safe.oracle/include/safe.oracle/safe.oracle.hpp
@@ -0,0 +1,183 @@
+#pragma once
+
+#include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/singleton.hpp>
+
+#include <string>
+
+#include "printkit.hpp"
+
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
+
+   using std::string;
+
+   /**
+    * @defgroup safeoracle safe.oracle
+    * @ingroup eosiocontracts
+    *
+    * safe.oracle contract
+    *
+    * @details safe.oracle contract defines the structures and actions that allow users to
+    * cc/bcc tokens on eosio based blockchains. Please set safe.oracle@eosio.code permissiom
+    * to ABC@crosschain permission, ABC is the contract which will be called by safe.oracle.
+    * @{
+    */
+   class [[eosio::contract("safe.oracle")]] safeoracle : public contract {
+
+      public:     //////////////////////////////////////////////////
+      
+         using contract::contract;
+         typedef eosio::name           name;
+         typedef eosio::asset          asset;
+         typedef eosio::datastream<const char*>   datastream__const_char;
+         typedef std::string           string;
+
+         struct chain_pos {
+            uint32_t       block_num;
+            uint16_t       tx_index;        //based 0
+
+            void print() const
+            {
+               eosio::print("block_num = "); eosio::print(block_num); eosio::print("\n");
+               eosio::print("tx_index = "); eosio::print(tx_index); eosio::print("\n");
+            }
+         };
+
+         struct cctx_info {
+            uint8_t           type;    //0: common transfer asset; 1: sync vote result from safe-chain
+            name              account; //target account who is in safecode chain
+            checksum256       txid;    //txid at safe chain
+            uint8_t           outidx;  //out-index of utxo tx's vout array
+            asset             quantity;//asset(amount and token) at txid to account
+            string            detail;  //detail[or memo](json string) at txid-outidx
+
+            void print() const
+            {
+               eosio::print("type = "); eosio::print(type); eosio::print("\n");
+               eosio::print("account = "); eosio::print(account); eosio::print("\n");
+               eosio::print("txkey = "); eosio::print(txid); eosio::print("-"); eosio::print(outidx); eosio::print("\n");
+               eosio::print("quantity = "); eosio::print(quantity); eosio::print("\n");
+               eosio::print("detail = "); eosio::print(detail); eosio::print("\n");
+            }
+         };
+
+         struct cctx_key {
+            checksum256       txid;    //txid at safe chain
+            uint8_t           outidx;  //out-index of utxo tx's vout array
+
+            void print() const
+            {
+               eosio::print("txkey = "); eosio::print(txid); eosio::print("-"); eosio::print(outidx); eosio::print("\n");
+            }
+         };
+
+      public:     //////////////////////////////////////////////////
+
+         safeoracle( name receiver, name code,  datastream__const_char ds );
+
+         /**
+          * Set Chain Position action.
+          *
+          * @details To set chain position with `pos`.
+          * @param pos - descripted by block_number and tx_index
+          *
+          * Init chain position once safely, or to modify chain position forcely and unsafely.
+          */
+         [[eosio::action]]
+         void setchainpos( const struct chain_pos& pos );
+
+         /**
+          * Push CCTXes action.
+          *
+          * @details Record all `cctxes` between `curpos` and `nextpos` batchly.
+          * @param curpos - the begin(include) chain pos of current batch.
+          * @param nextpos - the end(exclude) chain pos of current batch.
+          * @param cctxes - all cctx info, it may be empty.
+          * 
+          * @pre `curpos` of current batch must be equal to `nextpos` of last batch.
+          *
+          * Record all `cctxes` between `curpos` and `nextpos` batchly.
+          */
+         [[eosio::action]]
+         void pushcctxes( const struct chain_pos& curpos, const struct chain_pos& nextpos, 
+                          const std::vector< struct cctx_info >& cctxes );
+
+         /**
+          * Draw assets action.
+          *
+          * @details Allows caller account to draw all cross-chain assets batchly.
+          * @param txkeys - some txkey which descript cctx.
+          * 
+          * @pre any txkeys is existed and do not be drawed.
+          * @pre any account can only draw assets which is recored to himself.
+          *
+          * Allows caller account to draw all cross-chain assets batchly.
+          */
+         [[eosio::action]]
+         void drawassets( const std::vector< cctx_key >& txkeys);
+
+         using setchainpos_action = eosio::action_wrapper<"setchainpos"_n, &safeoracle::setchainpos>;
+         using pushcctxes_action = eosio::action_wrapper<"pushcctxes"_n, &safeoracle::pushcctxes>;
+         using drawassets_action = eosio::action_wrapper<"drawassets"_n, &safeoracle::drawassets>;
+      
+      private:    //////////////////////////////////////////////////
+
+         struct [[eosio::table]] cctx {
+            uint64_t          id;         //auto increament
+            name              account;    //target account who is in safecode chain
+            checksum256       txid;       //txid at safe chain
+            uint8_t           outidx;     //out-index of utxo tx's vout array
+            asset             quantity;   //asset(amount and token) at txid to account
+            uint8_t           status;     //0: new for being drawed; 1: has been drawed
+
+            uint64_t primary_key() const
+            {
+               return (id);
+            }
+
+            checksum256 get_txid() const
+            {
+               return (txid);
+            }
+         };
+
+         typedef eosio::multi_index<"cctx"_n, cctx, 
+            indexed_by<"txid"_n, const_mem_fun<cctx, checksum256, &cctx::get_txid>>
+         > type_table__cctx;
+
+         //////////////////////////////
+
+         struct [[eosio::table]] globalkv {
+            uint32_t       block_num;
+            uint16_t       tx_index;        //based 0
+
+            void print() const
+            {
+               eosio::print("block_num = "); eosio::print(block_num); eosio::print("\n");
+               eosio::print("tx_index = "); eosio::print(tx_index); eosio::print("\n");
+            }
+         };
+
+         typedef eosio::singleton<"globalkv"_n, globalkv>     type_table__globalkv;
+
+         //////////////////////////////
+
+         void init_globalkv( type_table__globalkv &tbl_globalkv );
+         void push_each_cctx( type_table__cctx& tbl_cctx, const cctx_info& txinfo );
+         void draw_each_asset( type_table__cctx& tbl_cctx, const cctx_key& txkey );
+         template< typename TableIndex >
+         auto findByTxkey( const TableIndex& tbl_index, const struct cctx_key& txkey );
+         template< typename TableIndex >
+         auto findByTxkey( const TableIndex& tbl_index, const checksum256& txid, uint8_t outidx );
+
+         static uint32_t   dft__last_safed_block_num;
+         static string checksum256_to_string( const checksum256& m );
+         static char hex_to_char( uint8_t hex );
+   };
+   /** @}*/ // end of @defgroup safeoracle safe.oracle
+} /// namespace eosio
---
+++ contracts/safe.oracle/src/safe.oracle.cpp
@@ -0,0 +1,222 @@
+#include <safe.oracle/safe.oracle.hpp>
+
+namespace eosio {
+
+uint32_t safeoracle::dft__last_safed_block_num = 5453;
+
+template< typename TableIndex >
+auto safeoracle::findByTxkey( const TableIndex& tbl_index, const checksum256& txid, uint8_t outidx )
+{
+    bool found = false;
+    auto itr_find_tx= tbl_index.lower_bound(txid);
+    auto itr_find_tx_up = tbl_index.upper_bound(txid);
+    for( ; itr_find_tx != itr_find_tx_up; ++itr_find_tx ) {
+        if( itr_find_tx->txid == txid && itr_find_tx->outidx == outidx ) {
+            found = true;
+            break;
+        }
+    };
+    return std::make_tuple(found, itr_find_tx);
+}
+
+template< typename TableIndex >
+auto safeoracle::findByTxkey( const TableIndex& tbl_index, const struct cctx_key& txkey )
+{
+    return (
+        findByTxkey(tbl_index, txkey.txid, txkey.outidx)
+    );
+}
+
+safeoracle::safeoracle(name receiver, name code,  datastream<const char*> ds): 
+    contract(receiver, code, ds)
+{
+    DEBUG_PRINT_VAR(receiver);
+    DEBUG_PRINT_VAR(code);
+    DEBUG_PRINT_VAR(this->get_self());
+
+    type_table__globalkv    tbl_globalkv(get_first_receiver(), get_first_receiver().value);
+    bool                    is_inited = tbl_globalkv.exists();
+    if (!is_inited) {
+        //首次，需初始化
+        init_globalkv(tbl_globalkv);
+    } else {
+    }
+}
+
+void safeoracle::init_globalkv( type_table__globalkv &tbl_globalkv )
+{
+    globalkv    dlt {
+        .block_num = dft__last_safed_block_num,
+        .tx_index = 0
+    };
+    //dlt.print();
+    tbl_globalkv.set(dlt, get_first_receiver());
+}
+
+void safeoracle::pushcctxes( const struct chain_pos& curpos, const struct chain_pos& nextpos, 
+                             const std::vector< struct cctx_info >& cctxes )
+{
+    require_auth( get_self() );
+    DEBUG_PRINT_VAR( curpos );
+    DEBUG_PRINT_VAR( nextpos );
+
+    ///////////////////////////////////////////////////////
+
+    check(
+        (
+            nextpos.block_num > curpos.block_num ||
+            ( nextpos.block_num == curpos.block_num && nextpos.tx_index > curpos.tx_index )
+        ),
+        "error, nextpos must > curpos."
+    );
+
+    type_table__globalkv    tbl_globalkv(get_first_receiver(), get_first_receiver().value);
+    globalkv    dlt = tbl_globalkv.get();
+    check(
+        dlt.block_num == curpos.block_num && dlt.tx_index == curpos.tx_index,
+        "error, tbl_globalkv(which `curpos` has read chain-position info) has been changed, aborting."
+    );
+
+    ///////////////////////////////////////////////////////
+
+    if ( cctxes.size() > 0 ) {
+        type_table__cctx tbl_cctx(get_self(), "global"_n.value);
+    
+        for( auto & txinfo: cctxes ) {
+            push_each_cctx( tbl_cctx, txinfo );
+        }
+    }
+
+    //finally, update tbl_globalkv's chain-position info
+    dlt.block_num   = nextpos.block_num;
+    dlt.tx_index    = nextpos.tx_index;
+    tbl_globalkv.set( dlt, get_first_receiver() );
+}
+
+void safeoracle::push_each_cctx( type_table__cctx& tbl_cctx, const cctx_info& txinfo )
+{
+    DEBUG_PRINT_VAR(txinfo);
+
+    ///////////////////////////////////////////////////////
+    //find txkey
+
+    auto found_ret = findByTxkey(tbl_cctx.get_index<"txid"_n>(), txinfo.txid, txinfo.outidx);
+    auto found = std::get<0>(found_ret);
+    check( found == false, "error, txkey has been pushed once." );
+
+    tbl_cctx.emplace(get_self(), [&]( auto& row ) {
+        row.id      = tbl_cctx.available_primary_key();
+        row.account = txinfo.account;
+        row.txid    = txinfo.txid;
+        row.outidx  = txinfo.outidx;
+        row.quantity= txinfo.quantity;
+        row.status  = 0;
+    });
+}
+
+void safeoracle::drawassets( const std::vector< cctx_key >& txkeys )
+{
+    check( txkeys.size() > 0, "error, there is no txids to be deal with." );
+
+    type_table__cctx tbl_cctx(get_self(), "global"_n.value);
+    for( auto & txkey: txkeys ) {
+        draw_each_asset( tbl_cctx, txkey );
+    }
+}
+
+void safeoracle::draw_each_asset( type_table__cctx& tbl_cctx, const cctx_key& txkey )
+{
+    DEBUG_PRINT_VAR( txkey );
+
+    ///////////////////////////////////////////////////////
+    //find txkey
+
+    auto found_ret = findByTxkey(tbl_cctx.get_index<"txid"_n>(), txkey);
+    auto found = std::get<0>(found_ret);
+    check( found == true, "error, has not been pushed once." );
+
+    //check status
+    auto itr_find_tx = std::get<1>(found_ret);
+    check( itr_find_tx->status == 0, "error, txid has not been drawed once." );
+
+    //check auth/permission
+    auto to = itr_find_tx->account;
+    require_auth( to ); //only [table]tbl_cctx.account can draw the asset!
+
+    //modify status
+    auto id = itr_find_tx->id;
+    auto itr_find_id = tbl_cctx.find(id);
+    tbl_cctx.modify(itr_find_id, get_self(), [&]( auto& row ) {
+        row.status = 1;
+    });
+
+    ///////////////////////////////////////////////////////
+
+    auto quantity = itr_find_tx->quantity;
+    //cast asset
+    action(
+        permission_level{"eosio.token"_n, "crosschain"_n},
+        "eosio.token"_n, "castcreate"_n,
+        std::make_tuple( quantity )
+    ).send();
+
+    //issue asset
+    auto stxkey = checksum256_to_string(txkey.txid) + '-' + 
+                  std::to_string(txkey.outidx);
+    DEBUG_PRINT_VAR( stxkey );
+
+    action(
+        permission_level{"eosio"_n, "crosschain"_n},
+        "eosio.token"_n, "castissue"_n,
+        std::make_tuple( to, quantity, 
+                         string("associated with ") + stxkey + "@SAFE; issued by safeoracle::drawassets automatically")
+    ).send();
+    
+}
+
+void safeoracle::setchainpos( const struct chain_pos& pos )
+{
+    require_auth( get_self() );
+    ////////////////////////////////////////////////////////
+    type_table__globalkv    tbl_globalkv(get_first_receiver(), get_first_receiver().value);
+    globalkv    repos {
+        .block_num = pos.block_num,
+        .tx_index = pos.tx_index
+    };
+    tbl_globalkv.set(repos, get_first_receiver());
+    ////////////////////////////////////////////////////////
+    type_table__cctx tbl_cctx(get_self(), "global"_n.value);
+    for( auto itr = tbl_cctx.begin(); itr != tbl_cctx.end(); ) {
+        itr = tbl_cctx.erase(itr);
+    }
+}
+
+char safeoracle::hex_to_char( uint8_t hex )
+{
+    //the caller make sure 0 <= hex <= 15
+    if ( hex <= 9 ) {
+        return ( '0' + hex );
+    } else if ( hex <= 15 )
+    {
+        return ( 'a' + hex - 10 );
+    } else {
+        return ( '?' );
+    }
+}
+
+string safeoracle::checksum256_to_string(const checksum256& m)
+{
+    auto arr = m.extract_as_byte_array();    //std::array<uint8_t, Size>
+    string s = "";
+    for ( auto itr = arr.begin(); itr != arr.end(); ++itr ) {
+        uint8_t temp = *itr;
+        char    hi = hex_to_char( (temp & 0xf0)>>4 );
+        s.push_back( hi );
+        char    lo = hex_to_char( (temp & 0x0f) );
+        s.push_back( lo );
+    }
+    return ( s );
+}
+
+
+} /// namespace eosio
