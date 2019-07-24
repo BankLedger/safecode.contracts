2d8bce5e7dc631df06e24aa7a63624b430f87792
danX-4q <chenxd@bankledger.com>
2019-07-24 17:43:25 +0800

    add: check macro to wrap code and msg.

---
+++ contracts/safe.oracle/include/safe.oracle/checkkit.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef __FILE_CHECKKIT_HPP__
+#define __FILE_CHECKKIT_HPP__
+
+#include <eosio/eosio.hpp>
+
+namespace eosio { namespace danX {
+
+}} //namespace danX, eosio
+
+////////////////////////////////////////////////////////////
+
+#define CHECK_FMT_STR(pred, code, fmt, args...) ({                  \
+    if(!pred){                                                      \
+        char    msg[1024] = {};                                     \
+        snprintf(msg, sizeof(msg)-1, "%d|%s %s %d|" fmt,            \
+                 code, __FILE__, __FUNCTION__, __LINE__, ##args);   \
+        eosio::check(pred, msg);                                    \
+    }                                                               \
+})
+
+#define CHECK_STR(pred, code, str) ({                               \
+    if(!pred){                                                      \
+        char    msg[1024] = {};                                     \
+        snprintf(msg, sizeof(msg)-1, "%d|%s %s %d|%s", code,        \
+                 __FILE__, __FUNCTION__, __LINE__, str.c_str());    \
+        eosio::check(pred, msg);                                    \
+    }                                                               \
+})
+
+#endif  //__FILE_CHECKKIT_HPP__
